f04f1da0e2aa8a69b94dfed7c4c62270cfa88666
Ben Behrman <bbehrman10@gmail.com>
2016-08-12 14:50:19 -0400

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,45 +11,39 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160811201101) do
-ActiveRecord::Schema.define(version: 20160811173336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "entries", force: :cascade do |t|
-    t.string   "body"
-    t.integer  "user_id"
+    t.string   "body",                        null: false
+    t.integer  "user_id",                     null: false
     t.integer  "prompt_id"
-    t.boolean  "is_private"
-    t.boolean  "is_read"
-    t.boolean  "can_respond"
-    t.string   "body",        null: false
-    t.integer  "user_id",     null: false
-    t.integer  "prompt_id"
-    t.boolean  "is_private",  null: false
-    t.boolean  "is_read",     null: false
-    t.boolean  "can_respond", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.boolean  "is_private",  default: false, null: false
+    t.boolean  "is_read",     default: false, null: false
+    t.boolean  "can_respond", default: false, null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.index ["prompt_id"], name: "index_entries_on_prompt_id", using: :btree
     t.index ["user_id"], name: "index_entries_on_user_id", using: :btree
   end
 
   create_table "prompts", force: :cascade do |t|
-    t.string   "question"
     t.string   "question",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "responses", force: :cascade do |t|
-    t.string   "body"
-    t.integer  "entry_id_id"
-    t.boolean  "is_read"
-    t.boolean  "can_respond"
+    t.string   "body",        null: false
+    t.integer  "entry_id",    null: false
+    t.integer  "user_id",     null: false
+    t.boolean  "is_read",     null: false
+    t.boolean  "can_respond", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["entry_id_id"], name: "index_responses_on_entry_id_id", using: :btree
+    t.index ["entry_id"], name: "index_responses_on_entry_id", using: :btree
+    t.index ["user_id"], name: "index_responses_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -68,23 +62,6 @@ ActiveRecord::Schema.define(version: 20160811173336) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.string   "body",        null: false
-    t.integer  "entry_id",    null: false
-    t.integer  "user_id",     null: false
-    t.boolean  "is_read",     null: false
-    t.boolean  "can_respond", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["entry_id"], name: "index_responses_on_entry_id", using: :btree
-    t.index ["user_id"], name: "index_responses_on_user_id", using: :btree
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username",        null: false
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
   end
 
 end
