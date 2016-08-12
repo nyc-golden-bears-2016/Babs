d6bbdd657adacb603cc0c8d58d5dbd8fc8b34537
Alex Bills <email": null>
2016-08-12 16:07:53 -0400

    added entries controller, routes for entries, and seeds for template user, entry, and prompt

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,6 +4,8 @@ class EntriesController < ApplicationController
     @entry = Entry.new(permit_params)
     @entry.user_id = current_user.id
     if request.xhr?
+      # set the viewer for a new entry
+      # @entry.message_in_a_bottle
       if @entry.save
         render json: @entry
       else
@@ -30,6 +32,28 @@ class EntriesController < ApplicationController
 
   def permit_params
     params.require(:entry).permit(:body)
+  end
+
+  # is message in a bottle?
+  def message_in_a_bottle
+    if !self.is_private?
+      self.find_random_user
+    end
+  end
+
+  # public or private?
+  def is_private?
+    self.is_private
+  end
+
+  # find a random user to receiver message
+  def find_random_user
+    total_users = User.last.id
+    begin
+      bottle_receiver = User.find(rand(1..total_users))
+    end until !user.nil?
+    self.viewer = bottle_receiver.id
+    self.save
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(username: "Dummy", password: "test12")
+
+entries = Entry.create(user_id: user.id,
+                       body: "You are amoral, corrupt, and depraved. You are cruel, hearltess, mean-spirited barbarous. You are trecharous, despicable, and vilely contemptible. You are a low-down seducer.")
+
+
+# prompts
+"When was the last time you asked for help?"
+""
