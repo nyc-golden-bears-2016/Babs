e3e85d5126056df6a9f3abdefe2c02a613051767
Ben Behrman <bbehrman10@gmail.com>
2016-08-12 10:29:56 -0400

    not sure if we should push these

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessor :login
-  has_many :responses
-  has_many :entries
-  validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
-  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  # has_many :responses
+  # has_many :entries
+  # validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
+  # validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
