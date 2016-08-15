5edcd20bbf949ed48605c1161a527718af4e462d
Ben Kudler <bkudler@gmail.com>
2016-08-15 15:30:07 -0400

    prompt is good

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'pry'
 gem 'faker'
 
 gem 'bcrypt'
+gem 'rufus-scheduler'
 
 
 # Use Redis adapter to run Action Cable in production
---
+++ config/initializers/scheduler.rb
@@ -0,0 +1,12 @@
+#
+# config/initializers/scheduler.rb
+
+require 'rufus-scheduler'
+
+# Let's use the rufus-scheduler singleton
+#
+s = Rufus::Scheduler.singleton
+
+
+# Stupid recurrent task...
+#
