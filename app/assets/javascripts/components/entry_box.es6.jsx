cc38b9c4891294c90c0a0596d06e596ba942252e
Ben Behrman <bbehrman10@gmail.com>
2016-08-18 09:00:04 -0400

    did some icon and layout stuff

--- app/assets/javascripts/components/entry_box.es6.jsx
+++ app/assets/javascripts/components/entry_box.es6.jsx
@@ -5,10 +5,12 @@ class EntryBox extends React.Component{
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state ={
       respond: false,
-      stream: false,
+      stream: true,
       personal: false,
-      respond: false,
-      postLongEnough: false
+      postLongEnough: false,
+      bottleButtonYes: "bottle-button-no",
+      respondButtonYes: "respond-button-no",
+      streamButtonYes: "stream-button-yes"
   }
 
     this.onPrivate = this.onPrivate.bind(this);
@@ -18,13 +20,28 @@ class EntryBox extends React.Component{
   };
 
 
-  onPrivate(){
+  onPrivate(event){
+    event.preventDefault();
     this.setState({ personal: !this.state.personal})
+    if(this.state.personal){
+      this.setState({bottleButtonYes: "bottle-button-no"})
+    } else {
+      this.setState({bottleButtonYes: "bottle-button-yes"})
+      this.setState({streamButtonYes: "stream-button-no"})
+      this.setState({stream:false})
+    }
   }
 
   onRespond(event){
     event.preventDefault();
     this.setState({ respond: !this.state.respond })
+    if(this.state.respond){
+      this.setState({respondButtonYes: "respond-button-yes"})
+      this.setState({streamButtonYes: "stream-button-no"})
+      this.setState({stream:false})
+    } else{
+      this.setState({respondButtonYes: "respond-button-no"})
+    }
   }
 
   onStream(event){
@@ -33,6 +50,13 @@ class EntryBox extends React.Component{
       this.setState({stream: true})
     } else{
       this.setState({stream:false})
+    }
+    if(this.state.stream){
+      this.setState({streamButtonYes: "stream-button-yes"})
+      this.setState({respondButtonYes: "respond-button-no"})
+      this.setState({bottleButtonYes: "bottle-button-no"})
+    } else{
+      this.setState({streamButtonYes: "stream-button-no"})
     }
     this.setState({respond:false})
     this.setState({personal:false})
@@ -78,11 +102,11 @@ class EntryBox extends React.Component{
         <section id="entry-box">
           <h4>{this.props.inspo.question}</h4>
           <form id="entry-form" onSubmit={this.handleSubmit}>
-            <textarea className={ this.state.postLongEnough + " " + this.state.textAreaSize } ref="entryTextarea" onKeyUp={this.longEnough} name="body" placeholder="Write something..."/><br/>
-            <input className="stream-link entry-post-button" type='submit' value='Post'/><br/>
-            <a id="bottle-button bot-but" onClick={this.onPrivate} href="">bottle</a>
-            <a id="respond-button" onClick={this.onRespond} href="/nowhere">respond</a>
-            <a id="stream-checkbox" onClick={this.onStream} href="/nowhere">♨</a>
+              <textarea className={ this.state.postLongEnough + " " + this.state.textAreaSize } ref="entryTextarea" onKeyUp={this.longEnough} name="body" placeholder="Write something..."/><br/>
+              <input className="stream-link entry-post-button" type='submit' value='Post'/><br/>
+              <a className={this.state.bottleButtonYes} id="bottle-button" onClick={this.onPrivate} href=""></a>
+              <a className={this.state.respondButtonYes} id="respond-button" onClick={this.onRespond}href=""></a>
+              <a className={this.state.streamButtonYes} id="stream-checkbox" onClick={this.onStream} href=""></a>
           </form>
         </section>
       </div>
